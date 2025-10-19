b802a7e1a8d6a511372b515e4600eb36d8062dc6
Merlin Mason <info@merlinmason.co.uk>
2025-10-19 01:22:09 +0100

    set viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Poetsen_One } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
@@ -10,7 +10,14 @@ export const metadata: Metadata = {
   title: "Square Fruit - Magical Maths Game",
   description: "A fun and magical maths game for kids",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export const viewport: Viewport = {
   themeColor: "#ffc0e3",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 const poetsenOne = Poetsen_One({
